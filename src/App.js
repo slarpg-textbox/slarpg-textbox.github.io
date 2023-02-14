35c0101de40dbd59f926f920b0ca5154dd1511de
stoulram <stoulram@gmail.com>
2023-02-13 23:46:56 -0500

    add placeholder image, text for character and dialogue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import { useState } from "react";
 import "./App.css";
+import placeholder from "./assets/placeholder.png";
 
 const App = () => {
-  // const [text, setText] = useState("");
+  const [character, setTextCharacter] = useState("");
+  const [dialogue, setTextDialogue] = useState("");
 
-  // const handleTextChange = (event) => {
-  //   setText(event.target.value);
-  // };
-
-  // return (
-  //   <div>
-  //     <input type="text" value={text} onChange={handleTextChange} />
-  //     <p>{text}</p>
-  //   </div>
-  // );
+  const handleTextCharacter = (event) => {
+    setTextCharacter(event.target.value);
+  };
+  const handleTextDialogue = (event) => {
+    setTextDialogue(event.target.value);
+  };
 
   return (
     <div>
-      <p>Hello World</p>
+      <div class="input">
+        Character:
+        <input type="text" value={character} onChange={handleTextCharacter} />
+        <br />
+        <br />
+        Dialogue:
+        <input type="text" value={dialogue} onChange={handleTextDialogue} />
+      </div>
+
+      <div class="container">
+        <div id="image">
+          <img src={placeholder} alt="placeholder" width="100" height="100" />
+        </div>
+        <div id="text">
+          <p id="subject">{character}</p>
+          <p id="description">{dialogue}</p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+
+  // return (
+  //   <div>
+  //     <p>Hello World</p>
+  //   </div>
+  // );
+};
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        View the code here{" "}
+        <a href="https://github.com/slarpg-textbox/slarpg-textbox.github.io">
+          https://github.com/slarpg-textbox/slarpg-textbox.github.io
+        </a>
+      </p>
     </div>
   );
 };
