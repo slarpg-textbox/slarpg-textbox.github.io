05339903cd9c1aea2dc2b85c9c4252b70f95beca
stoulram <stoulram@gmail.com>
2023-02-13 22:18:55 -0500

    update homepage link in package.json

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [text, setText] = useState("");
+  // const [text, setText] = useState("");
 
-  const handleTextChange = (event) => {
-    setText(event.target.value);
-  };
+  // const handleTextChange = (event) => {
+  //   setText(event.target.value);
+  // };
+
+  // return (
+  //   <div>
+  //     <input type="text" value={text} onChange={handleTextChange} />
+  //     <p>{text}</p>
+  //   </div>
+  // );
 
   return (
     <div>
-      <input type="text" value={text} onChange={handleTextChange} />
-      <p>{text}</p>
+      <p>Hello World</p>
     </div>
   );
 };
